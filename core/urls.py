ba88db383b12dc286c74495a0ba6bb5cb538b3b5
Denita Maria <denitamaria06@gmail.com>
2025-06-19 21:47:19 +0530

    initial commit

---
+++ core/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from .views import login_view, dashboard_view
+
+urlpatterns = [
+    path('', views.login_view, name='login'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('dip-yclp/', views.dip_yclp_view, name='dip_yclp'),
+    path('dip-mentee/', views.dip_mentee_view, name='dip_mentee'),
+    path('profile/', views.profile_view, name='profile'),
+    path('work-diary/', views.work_diary_view, name='work_diary'),
+    path('repository/', views.repository_view, name='repository'),
+    path('notification/', views.notification_view, name='notification'),
+    path('transcript/', views.transcript_view, name='transcript'),
+    path('settings/', views.settings_view, name='settings'),
+]
